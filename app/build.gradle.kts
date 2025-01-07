0a24357b62d50383a7f377a592edaa29d94d4ebf
Assianc <azure334792@gmail.com>
2025-01-07 22:32:15 +0800

    build(app): 更新应用版本号- 将版本号从 3.5.1 修改为 3.5.2 - 修改位于 app/build.gradle.kts 文件中

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 3
-        versionName = "3.5.1"
+        versionName = "3.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
