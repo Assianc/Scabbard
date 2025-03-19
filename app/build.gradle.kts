878189639b1400817ee23d1cba83b2ec56989480
Assianc <azure334792@gmail.com>
2025-03-19 22:27:23 +0800

    build(app): 更新应用版本号至 4.2.3
    - 在 build.gradle.kts 中更新 versionName 为 4.2.3
    - 在 UpdateChecker.kt 中更新 LANZOU_VERSION 为 4.2.3
    - 新增 activity_about.xml 和 activity_appearance.xml 布局文件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 4
-        versionName = "4.2.2"
+        versionName = "4.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
